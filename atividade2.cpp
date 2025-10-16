1b6cf21ff106fbfbf6fdf9ea6f120ced5e812133
prg-maker <df3296709@gmail>
2025-10-16 16:29:23 -0300

    .

---
+++ atividade2.cpp
@@ -0,0 +1,21 @@
+//Daniel Fernandes Silva 222008459
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+
+
+int main(){
+
+    int n ;
+    cin >> n ;
+    vector<int> xs;
+    for(int i = 0 ; i < n ; i++ ){
+        int c;
+        cin >> c;
+        xs.push_back(c)
+
+    }
+    
+}
